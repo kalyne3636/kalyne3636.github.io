0b6b7f9ddab960e33747a342af3c1197739d0bb6
kalyne3636 <50116998+kalyne3636@users.noreply.github.com>
2019-06-15 22:17:15 -0300

    Add files via upload

---
+++ Nova pasta/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1560647113,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowrightbutton-sheet0.png",
+		"images/arrowleftbutton-sheet0.png",
+		"images/backgoundanimed-sheet0.png",
+		"images/backgoundanimed-sheet1.png",
+		"images/ite2-sheet0.png",
+		"images/enemycontroller-sheet0.png",
+		"images/bottondestroyer-sheet0.png",
+		"images/enegyframeui-sheet0.png",
+		"images/enegyui-sheet0.png",
+		"images/tiledbackground.png",
+		"images/powerupcontroller-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
